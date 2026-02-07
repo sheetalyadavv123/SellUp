c1dff205c2549d04efd14e4f409c1b2c5106ff50
sheetalyadavv123 <sheetalyad7@gmail.com>
2026-02-08 03:47:03 +0530

    tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <h1 className="text-3xl font-bold text-red-500">
+      Tailwind is working 🚀
+    </h1>
   )
 }
 
