ce659ebe02aab6aed5405412417506e4ce40ab94
sheetalyadavv123 <sheetalyad7@gmail.com>
2026-03-07 01:04:18 +0530

    changes

--- frontend/src/components/Chatbox.jsx
+++ frontend/src/components/Chatbox.jsx
@@ -1,5 +1,5 @@
 import { Loader2Icon, X } from 'lucide-react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { clearChat } from '../app/features/chatSlice';
 import { format } from 'date-fns'
@@ -16,6 +16,11 @@ function Chatbox() {
   const [newMessage, setNewMessage] = useState("");
   const [isLoading, setIsLoading] = useState(true);
   const [isSending, setIsSending] = useState(false);
+
+  const messagesEndRef=useRef(null)
+  useEffect(()=>{
+    messagesEndRef.current?.scrollIntoView({behavior: "smooth"})
+    },[messages.length])
 
   const fetchChat = async () => {
     setChat(dummyChats[0]);
@@ -103,11 +108,31 @@ function Chatbox() {
               </div>
             ))
           )}
-
+          <div ref={messagesEndRef}/>
         </div>
-
+        {/*input area */}
+        {chat?.listing?.status==="active" ?
+        (
+            <form className='p-4 bg-[#1a1d2e] border-t border-gray-200 rounded-b-lg'>
+                <div className='flex items-end space-x-2'>
+                    <textarea placeholder='Type your messages...' className='
+                    flex-1 resist-none border-gray-300 rounded-lg px-4 py-2
+                    focus:outline-indigo-500 max-h-32' rows={1}/>
+                    <button type='submit' className='bg-indigo-600 hover:bg-indigo-700 text-white p-2.5 rounded-lg disabled:opacity-50 transition-colors'>
+                        {isSending ? <Loader2Icon className='w-5 h-5 animate-spin'/>
+                        :<Send className='w-5 h-5'/>}
+                    </button>
+                </div>
+            </form>
+        )
+        :
+        (
+            <div className='p-4 bg-[#1a1d2e] border-gray-200 rounded-b-lg'>
+                <p>{chat? `Listing is ${chat?.listing?.status}`: "Loading chat..."}</p>
+            </div>
+        )
+    }
       </div>
-
     </div>
   );
 }
